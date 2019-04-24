54c82951e38e73199a2cb5e8d44f7d49154867f5
alwaysbollu <always.bollu@gmail.com>
2019-04-24 08:48:49 -0400

    Initial commit

---
+++ App/Containers/Example/ExampleScreen.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Platform, Text, View, ActivityIndicator, Image } from 'react-native'
+import { connect } from 'react-redux'
+import { PropTypes } from 'prop-types'
+import ExampleActions from 'App/Stores/Example/Actions'
+import { liveInEurope } from 'App/Stores/Example/Selectors'
+import Style from './ExampleScreenStyle'
+import { Images } from 'App/Theme'
+import { Button, Header } from 'react-native-elements';
+
+/**
+ * This is an example of a container component.
+ *
+ * This screen displays a little help message and informations about a fake user.
+ * Feel free to remove it.
+ */
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu.',
+  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu.',
+})
+
+class ExampleScreen extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+    return (
+      <View style={Style.container}>
+        {this.props.userIsLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <Button title="Hey!" />
+        )}
+      </View>
+    )
+  }
+}
+
+ExampleScreen.propTypes = {
+  user: PropTypes.object,
+  userIsLoading: PropTypes.bool,
+  userErrorMessage: PropTypes.string,
+  fetchUser: PropTypes.func,
+  liveInEurope: PropTypes.bool,
+}
+
+const mapStateToProps = (state) => ({
+  user: state.example.user,
+  userIsLoading: state.example.userIsLoading,
+  userErrorMessage: state.example.userErrorMessage,
+  liveInEurope: liveInEurope(state),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchUser: () => dispatch(ExampleActions.fetchUser()),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ExampleScreen)
